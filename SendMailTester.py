b59c48af9c082184a213e632bd33713297c44650
Aaron Law <aaronlaw@gmail.com>
2011-03-20 16:07:13 +0800

    add encoding at head & add testing code about MailServer

--- SendMailTester.py
+++ SendMailTester.py
@@ -1,3 +1,11 @@
+# -*- coding: utf8 -*-
+
 from SendMail import SmartMessage
 msg = SmartMessage("Me <me@example.com>", "You <you@example.com>", "Your picture","Here's that picture I took of you.")
 print (str(msg))
+
+
+from SendMail import MailServer
+msg1 = SmartMessage("aaronishere@gmail.com", "Aaron <aaronishere@gmail.com>, Hello <hello@abc.com>", 'This is subject', 'Content goes here.')
+print(str(msg1))
+MailServer('smtp.gmail.com', 'aaronishere@gmail.com','06936841', '465').sendMessage(msg1)
