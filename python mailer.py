14ea397cf51f84ae853c183bc8ca2455421396a5
Aaron Law <aaronlaw@gmail.com>
2011-03-20 15:27:25 +0800

    Version 1.0.2: Able to send out HTML message now

--- python mailer.py
+++ python mailer.py
@@ -11,24 +11,22 @@
 # Version 1.0.1:Pratical test.
 #		1. Setting of Gmail's SMTP server is tested (smtp.gmail.com:465 over SSL). Classes SmartMessage & MailServer are not in use temply due to Gmail's SSL problem.
 #		2. Test for sending out email to a receiver in a giver time interval automatically (e.g. send out a mail every 5 seconds)
+# Version 1.0.2: Able to send out HTML message now
 
 
 #TODO:	1. [x] Fix encoding problem (unicode support)
 #	2. [x] Do pratical test with Gmail (Can I use Gmail to send out email?)
 #	3. [x] Repeat mailing (Can I send out a mail to a person, 180 times in a 5 sec interval?)
 #	4. [] Change of program behavior: Send out email from an internal email-address-list (more than 1 receiver) in a single loop, one receiver each time
-#	5. [] Able to send out text/html message rather than text/plain only
+#	5. [x] Able to send out text/html message rather than text/plain only
 #	6. [] Read email addresses from a text-based mailing list
 #	7. [] Message template: Able to read external file as the content (html) of the message body
+#	8. [] Warp up: Tidy up code section to let others use my program...easily (May be warp up with function)
 ##############################################################
 import sys, time # taking argv and sleeper
 import smtplib # for SMTP mailing features
- 
-counter = 0
 
 ###### Variable Definition ######
-intervalPerAction = 10 # in second
-intervalPerBunch = 10*60 # min = time*60sec
 
 #@see help(smtplib)
 smtpAddr = 'smtp.gmail.com' #@see REFERENCE section
@@ -36,8 +34,11 @@ port = '465'
 username = 'aaronishere@gmail.com'
 password = '06936841'
 fromAddr = 'test@test.com'
-toAddr = ['gethighprofit@gmail.com','Aaron <aaronishere@gmail.com>', 'aaronlaw@gmail.com',  'herbalifeaaron@yahoo.com.hk']
-overSSL = True
+toAddr = ['gethighprofit@gmail.com','Aaron <aaronishere@gmail.com>']#, 'aaronlaw@gmail.com',  'herbalifeaaron@yahoo.com.hk', 'Kaiser KS <wwwkaiserkscom@gmail.com>', 'luk Benny <lukkaihang@hotmail.com>']
+isOverSSL = True
+
+intervalPerAction = 5 # in second
+intervalPerBunch = 10*60 # min = time*60sec
 #################################
 
 ###### REFERENCE ######
@@ -51,28 +52,46 @@ overSSL = True
 #TODO: http://www.wrox.com ISBN is 978-0-470-41463-7
 # http://www.wrox.com/dynamic/books/download.aspx 
 
+
+### for used by smtplib simply
+#msg = "Subject: Hello, a test from aaron's self made program\n\n"
+#msg = msg + "This is the body of the message\n 我們BBC’s Top 100 Best Novels BBC：有史以来最伟大的100部小说.有效的學習方法.\n<a href='http://www.google.com>A URL</a> port 465 by SMTP_SSL interval test"
+
+### for used by class SmartMessage
 from SendMail import SmartMessage, MailServer
-#msg = SmartMessage("Me <aaronishere@gmail.com>", 
-#		"You <aaronishere@gmail.com>",
-#		"Your picture","Here's that picture I took of you.")
-#MailServer(smtpAddr, username, password, port).sendMessage(msg)
 
-msg = "Subject: Hello, a test from aaron's self made program\n\n"
-msg = msg + "This is the body of the message\n 我們BBC’s Top 100 Best Novels BBC：有史以来最伟大的100部小说.有效的學習方法.\n<a href='http://www.google.com>A URL</a> port 465 by SMTP_SSL interval test"
+subject = "hello, this is a content-type test from aaron"
+msg = SmartMessage(fromAddr, 
+		toAddr,
+		subject,
+		"Dear all, <br />I am writing a mailing program and doing a test. please DO REPLY me if you got this email.(just press the REPLY BUTTON to let me see the actual mail)<br /><br />This is the body of the message\n 我們BBC’s Top 100 Best Novels BBC：有史以来最伟大的100部小说.有效的學習方法.\n<a href='http://www.google.com'>A URL</a> port 465 by SMTP_SSL interval test. Content-type = text/html<br />python mailer v1.0.2")
+# change the content-type to enable HTML
+msg.set_type('text/html')
+#print msg.get_content_type()
+msg = str(msg) # make SmartMessage object to string if not use MailServer to send mail
 
-server = smtplib.SMTP_SSL(smtpAddr, port)
+### use python built-in smtplib to connect to server
+if isOverSSL is True: # server connected over SSL
+	server = smtplib.SMTP_SSL(smtpAddr, port)
+
+else:
+	server = smtplib.SMTP(smtpAddr, port)
 server.login(username, password)
 server.sendmail(fromAddr, toAddr, msg)
+
+### use custom class MailServer to connect to server
+#MailServer(smtpAddr, username, password, port).sendMessage(msg)
 
 ##### MAIN ######
 try: 
 	#while counter <= len(sys.argv[1]):
 	for count in range(int(sys.argv[1])): #loop through the range in the cmd argv given
-		print 'Mail #',count ,'is sent.'
 		msgCount = "\n\nThis is the "+ str(count) +" out of "+ str(sys.argv[1]) +" mail, with a time interval = " + str(intervalPerAction) + "seconds."
-		#counter= counter+1
+		
 		server.sendmail(fromAddr, toAddr, msg+msgCount)
 		time.sleep(intervalPerAction)
+		
+		print 'Mail #',count ,'is sent.'
 except Exception as error: 
 	print error
 	print ('There was no such file.', error)
