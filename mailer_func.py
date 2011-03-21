ad46b1c4823837e04b88b8de6b2be1342d0fd7d6
Aaron Law <aaronlaw@gmail.com>
2011-03-21 08:14:36 +0800

    Make the program simulating human action...email bulk mails like a human-being. 	add function pauseInRandom() to simulate human's delay 	add function delayLikeAHuman() for pausing in every mail 	add function randomString() simply to make the content of email different in each send 	changes in sendMassMail(): the interval for sleep is an average number now.

--- mailer_func.py
+++ mailer_func.py
@@ -3,6 +3,7 @@
 import sys, time # taking argv and sleeper
 import smtplib # for SMTP mailing features
 from SendMail import SmartMessage, MailServer
+import random # simulate human action...pauseLikeAHuman()
 
 def readTxt(fname):
 	# I don't handle exception here (Cannot open a mailing list is not critical of the process of automation of mail-sending)
@@ -15,9 +16,26 @@ def readTxt(fname):
 		print ('Oh! Program is going to terminate')		
 		sys.exit()
 
+#def shuffleList(file):
+
+#def writeTxt(fname):
+
+### simulate human's delay
+def pauseInRandom(sleep):
+	min = sleep / 2
+	max = sleep * 2
+	return random.randint(min, max)
+
+def delayLikeAHuman(sleep):
+	time.sleep(pauseInRandom(sleep))
+
+### randomString() is put into the content of mail...to make every mail unique...to confuse gmail.
+def randomString():
+	return str(random.randint(777,7777))
+
 ### sendMail() sends one mail from a list (one mail to a bunch of people each time)
 ### It accepts a bunch of address in a list (e.g. ['1@test.com', '2@test.com'])
-def sendMail(subject, content, fromAddr, toAddrList, smtpAddr, username, password, port, sleep):
+def sendMail(subject, content, fromAddr, toAddrList, smtpAddr, username, password, port):
 	isSuccess = False	
 	try:
 		msg = SmartMessage(fromAddr, toAddrList, subject, content)
@@ -28,7 +46,7 @@ def sendMail(subject, content, fromAddr, toAddrList, smtpAddr, username, passwor
 		#print(msg)
 		MailServer(smtpAddr, username, password, port).sendMessage(msg)
 
-		time.sleep(sleep)
+		#time.sleep(sleep)
 		isSuccess = True	
 	except (Exception) as error: 
 		#print (error)
@@ -38,17 +56,19 @@ def sendMail(subject, content, fromAddr, toAddrList, smtpAddr, username, passwor
 	return isSuccess # return True when mail send successfully
 
 ### sendMassMail() does sending mail to a receiver once a time
+### mailList is accept a file-handler or [1@test.com, 2@test.com]
 def sendMassMail(subject, content, fromAddr, mailList, smtpAddr, username, password, port, sleep=5):
 	count = 0
 	totalOfAddr = 0
-	intervalPerBunch = sleep*60 # min = time*60sec
-	print 'I am in mass mail mode. I am goint to send mail in every '+ str(sleep) + ' second.'
+	#intervalPerBunch = pauseInRandom(sleep)*60 # min = time*60sec # move downward, not in here...see a bug?
+	print 'I am in mass mail mode. I am goint to send mail in average every '+ str(sleep) + ' second.'
+	sendMail('Notice: '+subject+ ' from '+ fromAddr, content, fromAddr, 'gethighprofit@gmail.com', smtpAddr, username, password, port)
 	for toSingleAddr in mailList:
-
 		print 'Processing ' + toSingleAddr + '...'
 		toAddr = toSingleAddr.rstrip("\r\n")  # work around: the ending '\n' char in an address leads to a bug. That bug is 'text\html' doesn't work and leads to the outing mail being plain text
 					# Reference: Google: python remove \n
- 		isSuccess = sendMail(subject, content, fromAddr, toAddr, smtpAddr, username, password, port, sleep)
+ 		isSuccess = sendMail(subject, content, fromAddr, toAddr, smtpAddr, username, password, port)
+		delayLikeAHuman(sleep)
 		if isSuccess is True:
 			count = count + 1 # count is up only when mail out was success (@see isSuccess in sendMail())
 			print 'Mail #',count,' is sent to ' + toSingleAddr + '.'
@@ -58,16 +78,19 @@ def sendMassMail(subject, content, fromAddr, mailList, smtpAddr, username, passw
 		totalOfAddr = totalOfAddr + 1 # totalOfAddr is up whenever mail out was success or not
 		# pause every 30 mail
 		if totalOfAddr % 30 == 0:
-			print('I am going to sleep ',intervalPerBunch, 'seconds.')
-			time.sleep(intervalPerBunch)
+			intervalPerBunch = pauseInRandom(sleep)*60 # min = time*60sec 
+			print('So tired...I have to take a rest for ',intervalPerBunch/60, 'minutes.')
+			delayLikeAHuman(intervalPerBunch)
+			print('OK, I feel better now.')
 	print('There are ', count, ' out of', totalOfAddr, 'mail sent successfully.')
 	print('If you found a bug or an idea, pls report to aaronlaw@gmail.com. Thanks for using it, bye!' )
 
+### send mail to a list of receivers repeatly
 ### mailList is a list of receivers
 def sendRepeatMail(subject, content, fromAddr, toAddrList, smtpAddr, username, password, port, sleep=5, repeat=1):
 	print 'I am in repeat mail mode. I am goint to send mail in every '+ str(sleep) + ' second.'
 	for i in repeat:
 		print 'Repeat mail round #', i
 		print 'Processing... ' + toSingleAddr,
-		sendMail(subject, content, fromAddr, toAddrList, smtpAddr, username, password, port, sleep)
+		sendMail(subject, content, fromAddr, toAddrList, smtpAddr, username, password, port)
 
